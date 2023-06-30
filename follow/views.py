e2973e98f5ddf1471ea5b9517e5e69b73c8181fc
joão vitor ribeiro <twiceqbit@gmail.com>
2023-06-30 11:43:07 -0300

    feat: follow model/serializer/view

--- follow/views.py
+++ follow/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import Follow
+from books import Book
+from users import User
+from .serializer import FollowSerializer
+from .permissions import IsAccountOwner
+from rest_framework.permissions import (
+    IsAdminUser,
+)
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class FollowView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner | IsAdminUser]
+
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+    def perform_create(self, serializer):
+    book = get_object_or_404(Book, pk=self.kwargs["pk"])
+    serializer.save(book=book, user=self.request.user)
--- loan/views.py
+++ loan/views.py
@@ -1,16 +1,19 @@
 from rest_framework.generics import CreateAPIView
 from .models import Loan
 from .serializer import LoanSerializer
-
+from rest_framework.permissions import (
+    IsAdminUser,
+)
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class LoanView(CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
 
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
     def perform_create(self, serializer):
-        user_id = self.request.data.get('user_id')
-        user = get_object_or_404(User,id=user_id)
         copy_id = self.request.data.get('user_id')
         copy = User.objects.get(id=user_id)
-        serializer.save(user=user, copy=copy)
+        serializer.save(user=self.request.user, copy=copy)
