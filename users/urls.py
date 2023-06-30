4c44bb342d258518c6b6f0337ae3c8cfe42103ac
Zevitorb <benvenuttijosevitor@gmail.com>
2023-06-30 12:53:15 -0300

    fix: user detail url

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
-    path("users/<int:pk>/", views.UserView.as_view()),
+    path("users/<int:pk>/", views.UserDetailView.as_view()),
     path("users/login/", TokenObtainPairView.as_view()),
     path("users/refresh/", TokenRefreshView.as_view()),
 ]
