c6800eb49eb58d1574d2c4b5a4d0cde07334a521
carla <carla.zollaa@gmail.com>
2023-06-30 14:16:43 -0300

    feat: listCreateView Book

--- book/serializer.py
+++ book/serializer.py
@@ -1,17 +1,11 @@
 from rest_framework import serializers
 from .models import Book
 
-# from users.models import User
-
-# from users.serializer import UserSerializer
-
 
 class BookSerializer(serializers.ModelSerializer):
-    # user = UserSerializer(read_only=True)
-
     class Meta:
         model = Book
-        field = ["id", "autor", "title", "user_id"]
+        fields = ["id", "autor", "title", "user_id"]
 
     def create(self, validated_data):
         return Book.objects.create(**validated_data)
