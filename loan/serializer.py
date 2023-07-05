25e680aca67d395ac8bc9ff9e59d3a68aa2aad1f
joão vitor de carvalho ribeiro <twiceqbit@gmail.com>
2023-07-05 11:51:12 -0300

    Revert " feat: fix loan view"

--- loan/serializer.py
+++ loan/serializer.py
@@ -9,25 +9,28 @@ from copies.models import Copy
 class LoanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Loan
-        fields = ["id", "user", "copy", "date", "prazo", "devolvido"]
-        read_only_fields = ["id", "date", "prazo", "devolvido", "prazo"]
+        fields = ["id", "user", "copy", "date", "term", "returned"]
+        read_only_fields = ["id", "date", "returned", "term"]
 
     def create(self, validated_data):
         return Loan.objects.create(**validated_data)
 
     def update(self, instance: Loan, validated_data: dict) -> Loan:
-        loan = Loan.objects.get(id=validated_data["loan_id"])
-        loan.devolvido = True
-        loan.save()
-
         copy = Copy.objects.get(id=validated_data["copy_id"])
-        copy.disponibilidade = True
+        copy.availability = True
         copy.save()
 
-        if validated_data["block"]:
-            user = User.objects.get(id=validated_data["user_id"])
+        loan = Loan.objects.get(id=copy.loan)
+        loan.returned = True
+        loan.save()
 
-            current_date = timezone.now()
+        current_date = timezone.now()
+        print(current_date > loan.term)
+
+        if current_date > loan.term:
+            user = User.objects.get(id=loan.user_id)
+            user.block = True
+
             prazo = current_date + timedelta(days=7)
             user.timeBlock = prazo
             user.save()
--- loan/views.py
+++ loan/views.py
@@ -24,29 +24,32 @@ class LoanView(generics.CreateAPIView):
         copy_id = self.request.data.get("copy")
         copy = Copy.objects.get(id=copy_id)
 
-        if copy.disponibilidade == False:
-            raise ValidationError("A cópia não está disponível.")
+        if copy.availability == False:
+            raise ValidationError("Copy is not available.")
 
         user_id = self.request.data.get("user")
         user = User.objects.get(id=user_id)
 
         if user.block == True:
-            raise ValidationError("O usuário está bloqueado.")
+            raise ValidationError("The user is blocked.")
 
-        copy.disponibilidade = False
+        copy.availability = False
         copy.save()
 
-        prazo = self.calculate_prazo()
-        serializer.save(user=user, copy=copy, prazo=prazo)
+        term = self.calculate_prazo()
+        loan = serializer.save(user=user, copy=copy, term=term)
+
+        copy.loan = loan.id
+        copy.save()
 
     def calculate_prazo(self):
         current_date = timezone.now()
-        prazo = current_date + timedelta(days=7)
+        term = current_date + timedelta(days=7)
 
-        if prazo.weekday() >= 5:
-            prazo += timedelta(days=2)
+        if term.weekday() >= 5:
+            term += timedelta(days=2)
 
-        return prazo
+        return term
 
 
 class LoanDetailView(generics.RetrieveUpdateAPIView):
@@ -57,13 +60,6 @@ class LoanDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = LoanSerializer
 
     def perform_update(self, serializer):
-        copy_id = self.request.data.get("copy")
-
-        user_id = self.request.data.get("user")
-
         return serializer.save(
-            user_id=user_id,
-            copy_id=copy_id,
-            loan_id=self.kwargs.get("pk"),
-            block=self.request.data.get("block"),
+            copy_id=self.kwargs.get("pk"),
         )
