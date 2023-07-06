cb5540051bd8d76b0121a6de3f0ed6de0cb876c4
joão vitor ribeiro <twiceqbit@gmail.com>
2023-07-06 15:35:44 -0300

    fix: teste 3 deploy

--- _core/settings.py
+++ _core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dotenv
+import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
 dotenv.load_dotenv()
